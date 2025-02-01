2551c721b9c7aa0b6ba788d72a5b5b7251b30b35
Maksim Rozhkov <kermit@mail.ru>
2025-02-01 10:28:55 +0300

    Добавлена отправка JSON на внешний сервер

---
+++ CountByRequest/Utilites/PostRequest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace CountByRequest.Utilites
+{
+    public class PostRequest
+    {
+        public string Currency { get; set; } = "USD";
+        public int Notes { get; set; }
+        public int Denomination { get; set; }
+    }
+
+    public class PostSender
+    {
+        private readonly HttpClient _httpClient;
+
+        public PostSender()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task SendPostAsync(PostRequest postRequest, string url)
+        {
+            try
+            {
+                // Сериализуем экземпляр класса PostRequest в текстовую строку, содержащую JSON
+                string jsonString = JsonSerializer.Serialize(postRequest, new JsonSerializerOptions { WriteIndented = true });
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                // Посылаем post-запрос по указанному URL
+                var response = await _httpClient.PostAsync(url, content);
+
+                // Проверяем результат - удалось ли доставить запрос на сервер
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Post request sent successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"Error sending post request: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred: {ex.Message}");
+            }
+        }
+    }
+}
+
+
--- CountByRequest/Views/MainView.axaml.cs
+++ CountByRequest/Views/MainView.axaml.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Interactivity;
+using CountByRequest.Utilites;
 using CountByRequest.ViewModels;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CountByRequest.Views;
 
@@ -21,7 +24,27 @@ public partial class MainView : UserControl
             viewModel.IsButtonEnabled = false;
             viewModel.Message = "Done";
 
-            // TODO: посылаем JSON с результатами пересчёта на банковский сервер
+            // Посылаем JSON с результатами пересчёта на банковский сервер.
+            
+            // TODO: сейчас мы не ждём завершения запроса, но нам будет нужно
+            // уведомить оператора о том, получилось ли выполнить операцию, или нет
+            Thread postRequestThread = new Thread(() => SendPost());
+            postRequestThread.Start();
         }
+    }
+
+    public async Task SendPost()
+    {
+        var poseSender = new PostSender();
+        var poseRequest = new PostRequest
+        {
+            Currency = "USD",
+            Notes = 50,
+            Denomination = 100
+        };
+
+        // Предполагаем, что сервер находится на этом же компьютере и слушает порт 5000
+        string url = "http://localhost:5000";
+        await poseSender.SendPostAsync(poseRequest, url);
     }
 }
